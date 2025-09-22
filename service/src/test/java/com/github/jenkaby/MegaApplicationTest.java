3aec560e33f45209ea7af922f7832b7ceac36db9
yauhen.k <evgen.kuzmich@gmail.com>
2025-09-22 14:26:44 +0300

    test: fix the startup test config

--- service/src/test/java/com/github/jenkaby/MegaApplicationTest.java
+++ service/src/test/java/com/github/jenkaby/MegaApplicationTest.java
@@ -2,7 +2,12 @@ package com.github.jenkaby;
 
 import com.github.jenkaby.support.db.AbstractReusableDbTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBeans;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
@@ -10,7 +15,15 @@ import org.springframework.test.context.ActiveProfiles;
 class MegaApplicationTest extends AbstractReusableDbTest {
 
     @Test
+    @Timeout(30)
     void contextLoads() {
 
+    }
+
+    @TestConfiguration
+    @MockBeans({
+            @MockBean(classes = JwtDecoder.class)
+    })
+    static class TestConfig {
     }
 }
