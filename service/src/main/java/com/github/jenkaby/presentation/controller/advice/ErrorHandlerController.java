c3636faf4ffbd2ac26ce30561edbc25b8ef182f9
yauhen.k <evgen.kuzmich@gmail.com>
2025-09-22 10:13:05 +0300

    improve: handle NoResourceFoundExteption properly

--- service/src/main/java/com/github/jenkaby/presentation/controller/advice/ErrorHandlerController.java
+++ service/src/main/java/com/github/jenkaby/presentation/controller/advice/ErrorHandlerController.java
@@ -2,10 +2,13 @@ package com.github.jenkaby.presentation.controller.advice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice
@@ -23,5 +26,12 @@ public class ErrorHandlerController {
     public String authorizationDeniedException(AuthorizationDeniedException authzDeniedException) {
         log.warn("The logged in user has insufficient permissions: {}", authzDeniedException.getAuthorizationResult());
         return authzDeniedException.getAuthorizationResult().toString();
+    }
+
+    //    special handle 500 error for favicon.ico requests. Actually it can throw any status code
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ProblemDetail> handleNoResourceFound(NoResourceFoundException ex) {
+        log.warn("The resource '{}' not found by reason: {}", ex.getResourcePath(), ex.getMessage());
+        return new ResponseEntity<>(ex.getBody(), ex.getStatusCode());
     }
 }
