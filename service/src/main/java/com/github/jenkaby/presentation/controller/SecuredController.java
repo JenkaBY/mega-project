4cea25c6264d6ba8f48bd08b0c037e03f5125d92
yauhen.k <evgen.kuzmich@gmail.com>
2026-01-08 17:21:02 +0200

    feat: improve swagger for secured endpoints

--- service/src/main/java/com/github/jenkaby/presentation/controller/SecuredController.java
+++ service/src/main/java/com/github/jenkaby/presentation/controller/SecuredController.java
@@ -4,9 +4,18 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.jenkaby.config.security.support.LoggedUser;
 import com.github.jenkaby.config.security.support.LoggedUserInfo;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -23,12 +32,25 @@ import java.util.Map;
 @RequestMapping("/api/v1/secured-resources")
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "The secured endpoints",
+        externalDocs = @ExternalDocumentation(description = "See the readme file",
+                url = "https://github.com/JenkaBY/mega-project/blob/master/README.md#Manual-testing-secured-endpoints"))
 public class SecuredController {
 
     private final ObjectMapper mapper;
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
+    @Operation(
+            summary = "Check secured endpoint",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "successful response"),
+                    @ApiResponse(responseCode = "400", description = "Bad request",
+                            content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                                    schema = @Schema(implementation = ProblemDetail.class))
+                    )
+            }
+    )
     public Map<String, Object> check() {
         log.info("[GET] Requested a secured endpoint.");
         return Map.of(
@@ -58,6 +80,8 @@ public class SecuredController {
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/basic/has-roles/developer")
+    @Operation(summary = "Secured endpoint for user having the 'developer' role + Basic Auth.",
+            security = @SecurityRequirement(name = "basicAuth"))
     public Map<String, Object> onlyFoDevWithBasicAuth() {
         log.info("[GET] Requested a secured endpoint for user having the 'developer' role + Basic Auth.");
         return Map.of(
